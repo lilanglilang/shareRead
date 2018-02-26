cd1a1ccc3039d53b430849e602ca0a21ff935bff
lilang <11170370113@qq.com>
2018-02-26 17:27:12 +0800

    更新系统

--- read/shareRead/urls.py
+++ read/shareRead/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     url(r'^aboutus$', views.about),
     url(r'^applicant$', applicant.applicant),
     url(r'^applicant/applicate$', applicant.applicate),
-    url(r'^admin/applicant/list$', applicant.list),  # 获取HTML页面
-    url(r'^admin/applicant/detail', applicant.detail),  # 返回详细列表
+    url(r'^admin/applicant/list$', applicant.list),  # 返回详细列表
+    url(r'^admin/applicant/detail', applicant.detail),
     url(r'^admin/applicant/adopt$', applicant.adopt),  # 审核成功
     url(r'^admin/applicant/reject$', applicant.reject),  # 审核拒绝
     url(r'^admin/applicant/delete', applicant.deleteApplicant),  # 删除用户
--- read/shareRead/views.py
+++ read/shareRead/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Student
+import datetime
 # Create your views here.
 #定义公用模块的位置
 def index(request):
     # 爱心人士获取学生信息列表
     studentList = Student.objects.filter(status=0)
-    return render(request,'shareRead/students.html', {'studentList': studentList, 'size': len(studentList)})
+    meaningTime=datetime.datetime.now().year
+    return render(request,'shareRead/students.html', {'meaningTime':meaningTime,'studentList': studentList, 'size': len(studentList)})
 def about(request):
     return render(request,'shareRead/aboutus.html')
 def error(request):
