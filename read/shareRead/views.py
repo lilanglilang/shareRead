5f1068e3b4ec28346dbe6d8513de1195d74d9b04
lilang <11170370113@qq.com>
2018-03-07 12:59:03 +0800

    更新系统的显示

--- read/shareRead/views.py
+++ read/shareRead/views.py
@@ -10,9 +10,10 @@ import datetime
 #定义公用模块的位置
 def index(request):
     # 爱心人士获取学生信息列表
-    studentList = Student.objects.filter(status=0).order_by('school')
+    # studentList = Student.objects.filter(status=0).order_by('school')
     meaningTime=datetime.datetime.now().year
-    return render(request,'shareRead/students.html', {'meaningTime':meaningTime,'studentList': studentList, 'size': len(studentList)})
+    # return render(request,'shareRead/students.html', {'meaningTime':meaningTime,'studentList': studentList, 'size': len(studentList)})
+    return render(request,'shareRead/students.html',context={'meaningTime':meaningTime})
 def about(request):
     return render(request,'shareRead/aboutus.html')
 def error(request):
@@ -23,7 +24,7 @@ def regDiv(request):
     if request.is_ajax():
         cityList = Student.objects.values("schoolCity").annotate(cities=Count("schoolCity")).filter(status=0).order_by("schoolCity")
         regList = Student.objects.filter(status=0).order_by('schoolCity')
-        t = get_template('shareRead/regList.html')  # get the contex of html
-        content_html = t.render({'regList': regList,
-                                 'cityList': cityList})  # render the template to the local html you want,rather than return a variable
-        return HttpResponse(content_html)
+        # t = get_template('shareRead/regList.html')  # get the contex of html
+        # content_html = t.render({'regList': regList,'cityList': cityList})  # render the template to the local html you want,rather than return a variable
+        return render(request,template_name='shareRead/regList.html',context={'regList': regList,
+                                 'cityList': cityList})
--- read/test/test.py
+++ read/test/test.py
@@ -1,3 +1,1 @@
 # -*- coding: utf-8 -*-
-import datetime
-print(datetime.datetime.now().year)
